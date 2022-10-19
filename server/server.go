99af5a067650407247177ab77d3f30202245ab51
Litmee <1970432@qq.com>
2022-10-19 23:54:22 +0800

    update

--- server/server.go
+++ server/server.go
@@ -4,3 +4,16 @@ type Server interface {
 	// Run 启动方法
 	Run()
 }
+
+type ZipperServer struct {
+	// 服务名称
+	Name string
+	// ip 地址
+	Ip string
+	// 端口号
+	Port uint16
+}
+
+func (zs *ZipperServer) Run() {
+
+}
