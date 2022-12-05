99212f4ee3b120606a6326ace3e1e531a147c8a8
litmee <1970432@qq.com>
2022-12-05 14:37:22 +0800

    logo.go update

--- common/logo.go
+++ common/logo.go
@@ -4,4 +4,4 @@ package common
 const Banner = "                                                         \n _|_|_|_|_|  _|                                          \n       _|        _|_|_|    _|_|_|      _|_|    _|  _|_|  \n     _|      _|  _|    _|  _|    _|  _|_|_|_|  _|_|      \n   _|        _|  _|    _|  _|    _|  _|        _|        \n _|_|_|_|_|  _|  _|_|_|    _|_|_|      _|_|_|  _|        \n                 _|        _|                            \n                 _|        _|                            \n\n"
 
 // framework version information
-var zipperVersion = "	Zipper v0.1.8, go 1.19"
+var zipperVersion = "	Zipper v0.1.9, go 1.19"
